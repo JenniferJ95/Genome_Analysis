7b4b4adcfdc09757601f6a32096578df73460cb2
Jennifer Johansson <jennij@rackham2.uppmax.uu.se>
2020-04-23 11:03:58 +0200

    update bash script for mummerplot

--- codes/05_mummerplot.sh
+++ codes/05_mummerplot.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 01:00:00
+#SBATCH -t 02:30:00
 #SBATCH -J mummerplot
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user jennifer.johansson.8786@student.uu.se
@@ -13,4 +13,4 @@ module load bioinfo-tools
 module load MUMmer
 
 # Commands
-mummerplot /home/jennij/Genome_Analysis/analyses/02_genome_assembly/nucmer/out.delta
+mummerplot --png /home/jennij/Genome_Analysis/analyses/02_genome_assembly/nucmer/out.delta
