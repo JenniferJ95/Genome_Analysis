5e8d83b672a622cc5995a68aa8aa25536bed6904
JenniferJ95 <jennifer.johansson.95@outlook.com>
2020-05-05 17:12:50 +0200

    update 11_maker2.sh

--- codes/11_maker2.sh
+++ codes/11_maker2.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 04:00:00
+#SBATCH -t 02:00:00
 #SBATCH -J maker2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user jennifer.johansson.8786@student.uu.se
@@ -12,5 +12,21 @@
 module load bioinfo-tools
 module load maker/3.01.1-beta
 
-# Commands
+# Commands, commented/uncommented depending on what I run
+
+#maker
+
+#gff3_merge -d clear_SOAPdenovo_output_K49_master_datastore_index.log
+
+#maker2zff clear_SOAPdenovo_output_K49.all.gff
+
+#fathom genome.ann genome.dna -validate > snap_validate_output.txt
+
+#fathom genome.ann genome.dna -categorize 1000
+#fathom uni.ann uni.dna -export 1000 -plus 
+
+#forge ../export.ann ../export.dna
+
+#hmm-assembler.pl my_genome parameters > my_genome.hmm
+
 maker
