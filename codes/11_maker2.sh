b5d6576b6237a3c5e3b5af8fe9fe4387020f6837
JenniferJ95 <jennifer.johansson.95@outlook.com>
2020-05-13 20:21:06 +0200

    change 11_maker2.sh

--- codes/11_maker2.sh
+++ codes/11_maker2.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 02:00:00
+#SBATCH -t 00:30:00
 #SBATCH -J maker2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user jennifer.johansson.8786@student.uu.se
@@ -14,8 +14,10 @@ module load maker/3.01.1-beta
 
 # Commands, commented/uncommented depending on what I run
 
+# ********** Step 1 **********
 #maker
 
+# ********** Step 2 **********
 #gff3_merge -d clear_SOAPdenovo_output_K49_master_datastore_index.log
 
 #maker2zff clear_SOAPdenovo_output_K49.all.gff
@@ -29,4 +31,19 @@ module load maker/3.01.1-beta
 
 #hmm-assembler.pl my_genome parameters > my_genome.hmm
 
-maker
+# ********** Step 3 **********
+#maker
+
+# ********** Step 4 (repeat step 2) **********
+gff3_merge -d clear_SOAPdenovo_output_K49_master_datastore_index.log
+
+#maker2zff clear_SOAPdenovo_output_K49.all.gff
+
+#fathom genome.ann genome.dna -validate > snap_validate_output.txt
+
+#fathom genome.ann genome.dna -categorize 1000
+#fathom uni.ann uni.dna -export 1000 -plus
+
+#forge ../export.ann ../export.dna
+
+#hmm-assembler.pl my_genome parameters > my_genome.hmm
