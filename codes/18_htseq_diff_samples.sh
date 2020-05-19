d033c58ff8648078480e604b304616caa77525ad
Jennifer Johansson <jennifer.johansson.95@outlook.com>
2020-05-19 12:52:59 +0200

    bash 18

--- codes/18_htseq_diff_samples.sh
+++ codes/18_htseq_diff_samples.sh
@@ -13,39 +13,39 @@ module load bioinfo-tools
 module load htseq
 
 # Commands
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719013_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719013_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719013_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719013_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719013
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719014_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719014_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719014_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719014_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719014
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719015_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719015_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719015_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719015_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719015
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719016_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719016_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719016_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719016_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719016
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719017_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719017_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719017_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719017_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719017
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719018_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719018_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719018_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719018_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719018
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719204_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719204_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719204_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719204_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719204
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719206_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719206_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719206_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719206_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719206
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719207_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719207_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719207_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719207_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719207
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719208_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719208_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719208_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719208_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719208
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719209_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719209_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719209_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719209_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719209
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719211_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719211_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719211_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719211_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719211
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719212_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719212_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719212_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719212_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719212
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719213_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719213_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719213_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719213_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719213
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719214_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719214_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719214_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719214_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719214
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719241_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719241_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719241_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719241_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719241
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719242_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719242_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719242_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719242_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719242
 
-htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719266_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719266_tophat_out/accepted_hits.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz
+htseq-count -f bam -r pos -s yes -i ID --samout=sel2_SRR1719266_htseq_output /home/jennij/Genome_Analysis/analyses/05_differential_expression/tophat_different_samples/sel2_SRR1719266_tophat_out/paired_ends.bam /home/jennij/Genome_Analysis/data/additional_data/GCF_001595765.1_Mnat.v1_genomic.gff.gz > out_htseq_SRR1719266
 
